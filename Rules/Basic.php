c341dce5bafec4129665d8191b202615f36770d5
François Hodierne <francois@hodierne.net>
2011-08-21 21:12:21 +0200

    basic rules update

--- Rules/Basic.php
+++ Rules/Basic.php
@@ -25,11 +25,15 @@ class Bouncer_Rules_Basic
         // plus
              if ($name == 'safari'   && strpos($version, '5.') === 0)    $scores[] = array(1, 'Recent Browser');
         else if ($name == 'chrome'   && strpos($version, '12.') === 0)   $scores[] = array(1, 'Recent Browser');
+        else if ($name == 'chrome'   && strpos($version, '13.') === 0)   $scores[] = array(1, 'Recent Browser');
+        else if ($name == 'chrome'   && strpos($version, '14.') === 0)   $scores[] = array(1, 'Recent Browser');
         else if ($name == 'firefox'  && strpos($version, '4.') === 0)    $scores[] = array(1, 'Recent Browser');
         else if ($name == 'firefox'  && strpos($version, '5.') === 0)    $scores[] = array(1, 'Recent Browser');
+        else if ($name == 'firefox'  && strpos($version, '6.') === 0)    $scores[] = array(1, 'Recent Browser');
+        else if ($name == 'firefox'  && strpos($version, '7.') === 0)    $scores[] = array(1, 'Recent Browser');
         else if ($name == 'explorer' && strpos($version, '8.') === 0)    $scores[] = array(1, 'Recent Browser');
         else if ($name == 'explorer' && strpos($version, '9.') === 0)    $scores[] = array(1, 'Recent Browser');
-        else if ($name == 'opera'    && strpos($version, '10.') === 0)   $scores[] = array(1, 'Recent Browser');
+        else if ($name == 'opera'    && strpos($version, '11.') === 0)   $scores[] = array(1, 'Recent Browser');
 
         // minus
         else if ($name == 'explorer'  && strpos($version, '5.') === 0)   $scores[] = array(-2.5, 'Old Browser');
@@ -41,6 +45,11 @@ class Bouncer_Rules_Basic
         else if ($name == 'opera' && strpos($version, '8.') === 0)       $scores[] = array(-2.5, 'Old Browser');
         else if ($name == 'opera' && strpos($version, '7.') === 0)       $scores[] = array(-2.5, 'Old Browser');
         else if ($name == 'opera' && strpos($version, '6.') === 0)       $scores[] = array(-2.5, 'Old Browser');
+        else if ($name == 'chrome' && strpos($version, '8.') === 0)      $scores[] = array(-2.5, 'Old Browser');
+        else if ($name == 'chrome' && strpos($version, '7.') === 0)      $scores[] = array(-2.5, 'Old Browser');
+        else if ($name == 'chrome' && strpos($version, '6.') === 0)      $scores[] = array(-2.5, 'Old Browser');
+        else if ($name == 'chrome' && strpos($version, '5.') === 0)      $scores[] = array(-2.5, 'Old Browser');
+        else if ($name == 'chrome' && strpos($version, '4.') === 0)      $scores[] = array(-2.5, 'Old Browser');
         else if ($name == 'mozilla')                                     $scores[] = array(-2.5, 'Old Browser');
 
         return $scores;
@@ -59,8 +68,9 @@ class Bouncer_Rules_Basic
         $os_version = $identity['os'][1];
 
         // plus
-        if ($os_name == 'macosx' && strpos($os_version, '10.6') === 0) $scores[] = array(1, 'Recent OS');
-        else if ($os_name == 'windows7')                               $scores[] = array(1, 'Recent OS');
+        if ($os_name == 'macosx' && strpos($os_version, '10.6') === 0)     $scores[] = array(1, 'Recent OS');
+        elseif ($os_name == 'macosx' && strpos($os_version, '10.7') === 0) $scores[] = array(1, 'Recent OS');
+        else if ($os_name == 'windows7')                                   $scores[] = array(1, 'Recent OS');
 
         // minus
         else if ($os_name == 'windows95') $scores[] = array(-2.5, 'Old OS');
