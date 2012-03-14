1af82a42eb8015e4929b559a541703f4300f65ea
François Hodierne <francois@hodierne.net>
2012-03-14 09:51:01 +0100

    fg update

--- fingerprints/banned.php
+++ fingerprints/banned.php
@@ -37,6 +37,7 @@ $banned_fingerprints = array(
 'ed6a32ee2c78863ad5a1f007a95ae420', //  * - larbin 2.6.3
 'ab37081000071ae3ced2a659889930c3', //  * - larbin 2.6.3
 '27ed65dcbd6bad42936ac2b52e97cc63', //  * - larbin 2.6.3
+'993c22663746e77e424fd6c895ff0bdb', //  * - mozilla/5.0 larbin2.6.3@unspecified.mail
 '41690152a7d0c8c259a8854cc72b65f5', //  * - AutoHotkey
 // ICS
 '4c67cac030adba659c1ad691368db27e',
--- fingerprints/suspicious.php
+++ fingerprints/suspicious.php
@@ -27,6 +27,7 @@ $suspicious_fingerprints = array(
 '56e43867f63d0728e66efc91b717d3cc', // Mozilla/4.0 - Accept:*/* - Accept-Language:zh-cn, zh - Accept-Charset:GB2312,utf-8;q=0.7,*;q=0.7
 '982301312ae692bbf1eab93007cd823f', // Mozilla/4.0 (compatible;) - Accept:*/*, */* - Accept-Language:nl,en,*
 '6df194c3ff781bb9d84fca6a3ad4a5e1', // HttpClient
+'d2f2c09facd0de66d90f819d48366578', // Accept:*/* - Accept-Encoding:gzip,deflate
 // Programming languages defaults
 'd7c338fc5292ac8b15bacd0808ee1245', // Ruby
 'e92acd969162f34aaac2099466e58464', // Ruby/1.8.7
