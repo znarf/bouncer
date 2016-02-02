db00bf6054c87695856b8bcda7e277268c73771e
François Hodierne <francois@hodierne.net>
2016-02-02 23:42:19 +0100

    add wordpress cache class

---
+++ src/Cache/Wordpress.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Bouncer package.
+ *
+ * (c) François Hodierne <francois@hodierne.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bouncer\Cache;
+
+class Wordpress extends AbstractCache
+{
+
+    public function get($key)
+    {
+        return get_transient($key);
+    }
+
+    public function set($key, $value, $expire = 0)
+    {
+        set_transient($key, $value, $expire);
+    }
+
+}
