6e3f3c74ea598e1f226774bfc8992010ca7e3194
François Hodierne <francois@hodierne.net>
2016-05-09 11:07:53 +0200

    add timeout and improve documentation

--- src/Logger/LogstashLogger.php
+++ src/Logger/LogstashLogger.php
@@ -18,25 +18,49 @@ use Monolog\Logger;
 class LogstashLogger extends BaseLogger
 {
 
+    /**
+     * @var string
+     */
     protected $host;
 
+    /**
+     * @var int
+     */
     protected $port;
 
+    /**
+     * @var string
+     */
     protected $protocol;
 
+    /**
+     * @var string
+     */
     protected $channel;
 
+    /**
+     * @var string
+     */
     protected $type;
 
+    /**
+     * @var object
+     */
     protected $logger;
 
-    public function __construct($host, $port, $protocol = 'udp', $channel = 'bouncer', $type = 'access_log')
+    /**
+     * @var int
+     */
+    protected $timeout;
+
+    public function __construct($host, $port, $protocol = 'udp', $channel = 'bouncer', $type = 'access_log', $timeout = 2)
     {
         $this->host     = $host;
         $this->port     = $port;
         $this->protocol = $protocol;
         $this->channel  = $channel;
         $this->type     = $type;
+        $this->timeout  = $timeout;
     }
 
     /**
@@ -54,6 +78,9 @@ class LogstashLogger extends BaseLogger
         }
     }
 
+    /**
+     * @return object|null
+     */
     public function getLogger()
     {
         if (isset($this->logger)) {
@@ -65,7 +92,7 @@ class LogstashLogger extends BaseLogger
         $formatter = new LogstashFormatter(
             $this->type, $systemName = null, $extraPrefix = null, $contextPrefix = '', LogstashFormatter::V1);
 
-        $stream = stream_socket_client("{$this->protocol}://{$this->host}:{$this->port}", $errno, $errstr);
+        $stream = stream_socket_client("{$this->protocol}://{$this->host}:{$this->port}", $errno, $errstr, $this->timeout);
         if (!$stream) {
             error_log("Unable to connect to '{$this->protocol}://{$this->host}': {$errstr} ({$errno})");
             return $this->logger = false;
