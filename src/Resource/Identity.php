802a6cf5923986642eb48d7bc813ed58a400b3b2
François Hodierne <francois@hodierne.net>
2016-06-30 12:53:57 +0200

    isNice / getAgentName / getSystemName

--- src/Resource/Identity.php
+++ src/Resource/Identity.php
@@ -73,6 +73,9 @@ class Identity extends Resource
      */
     protected $reputation;
 
+    /**
+     * @param array $attributes
+     */
     public function __construct($attributes = null)
     {
         parent::__construct($attributes);
@@ -83,31 +86,49 @@ class Identity extends Resource
         }
     }
 
+    /*
+     * @return string|null
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /*
+     * @param string
+     */
     public function setId($id)
     {
         $this->id = $id;
     }
 
+    /*
+     * @return string|null
+     */
     public function getType()
     {
         return $this->type;
     }
 
+    /*
+     * @param string
+     */
     public function setType($type)
     {
         $this->type = $type;
     }
 
+    /*
+     * @return Address|null
+     */
     public function getAddress()
     {
         return $this->address;
     }
 
+    /*
+     * @param string|array|object $address
+     */
     public function setAddress($address)
     {
         if (is_object($address)) {
@@ -117,11 +138,17 @@ class Identity extends Resource
         }
     }
 
+    /*
+     * @return array
+     */
     public function getHeaders()
     {
         return $this->headers;
     }
 
+    /*
+     * @param array $headers
+     */
     public function setHeaders($headers)
     {
         $this->headers = $headers;
@@ -129,11 +156,17 @@ class Identity extends Resource
         $this->setSignature($signature);
     }
 
+    /*
+     * @return Signature|null
+     */
     public function getSignature()
     {
         return $this->signature;
     }
 
+    /*
+     * @param array|object $signature
+     */
     public function setSignature($signature)
     {
         if (is_object($signature)) {
@@ -143,11 +176,17 @@ class Identity extends Resource
         }
     }
 
+    /*
+     * @return UserAgent|null
+     */
     public function getUserAgent()
     {
         return $this->userAgent;
     }
 
+    /*
+     * @param array|object $userAgent
+     */
     public function setUserAgent($userAgent)
     {
         if (is_object($userAgent)) {
@@ -207,6 +246,14 @@ class Identity extends Resource
     /*
      * @return bool
      */
+    public function isNice()
+    {
+        return $this->getStatus() === Bouncer::NICE;
+    }
+
+    /*
+     * @return bool
+     */
     public function isSuspicious()
     {
         return $this->getStatus() === Bouncer::SUSPICIOUS;
@@ -220,6 +267,31 @@ class Identity extends Resource
         return $this->getStatus() === Bouncer::BAD;
     }
 
+    /*
+     * @return string|null
+     */
+    public function getAgentName()
+    {
+        $userAgent = $this->getUserAgent();
+        if ($userAgent) {
+            return $userAgent->getAgentName();
+        }
+    }
+
+    /*
+     * @return string|null
+     */
+    public function getSystemName()
+    {
+        $userAgent = $this->getUserAgent();
+        if ($userAgent) {
+            return $userAgent->getSystemName();
+        }
+    }
+
+    /*
+     * @return array
+     */
     public function toArray()
     {
         $identity = array();
--- src/Resource/UserAgent.php
+++ src/Resource/UserAgent.php
@@ -44,31 +44,49 @@ class UserAgent extends Resource
      */
     protected $system;
 
+    /*
+     * @return string|null
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /*
+     * @param string $id
+     */
     public function setId($id)
     {
         $this->id = $id;
     }
 
+    /*
+     * @return string|null
+     */
     public function getType()
     {
         return $this->type;
     }
 
+    /*
+     * @param string $type robot|browser
+     */
     public function setType($type)
     {
         $this->type = $type;
     }
 
+    /*
+     * @return object|null
+     */
     public function getAgent()
     {
         return $this->agent;
     }
 
+    /*
+     * @param object|array $agent
+     */
     public function setAgent($agent)
     {
         if (is_object($agent)) {
@@ -78,17 +96,45 @@ class UserAgent extends Resource
         }
     }
 
+    /*
+     * @return string|null
+     */
+    public function getAgentName()
+    {
+        $agent = $this->getAgent();
+        if ($agent) {
+            return $agent->getName();
+        }
+    }
+
+    /*
+     * @return object|null
+     */
     public function getSystem()
     {
         return $this->system;
     }
 
+    /*
+     * @param object|array $system
+     */
     public function setSystem($system)
     {
         if (is_object($system)) {
             $this->system = $system;
         } elseif (is_array($system)) {
             $this->system = new UserAgentPart($system);
+        }
+    }
+
+    /*
+     * @return string|null
+     */
+    public function getSystemName()
+    {
+        $system = $this->getSystem();
+        if ($system) {
+            return $system->getName();
         }
     }
 
